7b8ad5cb04d28af08f314643b16263f58ed207f1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-19 20:15:27 +0000

    Implemented the cocktail shaker sort algorithm

--- 1-main.c
+++ 1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	listint_t *list;
-	int array[] = {9, 12, 17, 25, 32, 47, 69, 94, 95};
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	list = create_listint(array, n);
--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -20,6 +20,83 @@ int sorted_list(listint_t *list)
 }
 
 /**
+ * forward_sort - Sort doubly linked list by moving forward
+ *
+ * @list_0: Doubly linked list
+ * @head: Head of doubly linked list passed
+ */
+void forward_sort(listint_t **head, listint_t **list_0)
+{
+	listint_t *list, *list_1 = NULL, *list_2, *list_3;
+
+	list = *list_0;
+	while (1)
+	{
+		if (list->next == NULL)
+		{
+			*list_0 = list;
+			return;
+		}
+		if (list->n <= list->next->n)
+			list = list->next;
+		else if (list->n > list->next->n)
+		{
+			list->prev != NULL ? list_1 = list->prev : NULL;
+			list_2 = list->next;
+			list_3 = list->next->next;
+			list_1 != NULL ? list_1->next = list_2 : NULL;
+			list_2->prev = list_1;
+			list_2->next = list;
+			list->prev = list_2;
+			list->next = list_3;
+			list_3 != NULL ? list_3->prev = list : NULL;
+			list_1 == NULL ? *head = list_2 : NULL;
+			print_list(*head);
+		}
+	}
+}
+
+/**
+ * backward_sort - Sort the doubly linked list by moving backward
+ *
+ * @list_0: Doubly linked list
+ * @tail: Tail of Doubly linked list
+ */
+void backward_sort(listint_t **list_0, listint_t **tail)
+{
+	listint_t *list, *list_1, *list_2, *list_3;
+
+	list = *tail;
+	while (1)
+	{
+		if (list->prev == NULL)
+		{
+			*tail = list;
+			return;
+		}
+		if (list->n >= list->prev->n)
+			list = list->prev;
+		else if (list->n < list->prev->n)
+		{
+			list_1 = list->next;
+			list_2 = list->prev;
+			list_3 = list->prev->prev;
+			list_2->next = list_1;
+			list_1->prev = list_2;
+			list->next = list_2;
+			list_2->prev = list;
+			list->prev = list_3;
+			if (list_3 != NULL)
+				list_3->next = list;
+			else
+				*list_0 = list;
+			print_list(*list_0);
+
+		}
+	}
+}
+
+/**
  * cocktail_sort_list - Sort a doubly linked list using
  * cocktail sort algorithm
  *
@@ -27,52 +104,14 @@ int sorted_list(listint_t *list)
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *t_0 = NULL, *t_1, *t_2, *t_3;
-	int ctrl = 1;
+	listint_t *temp = NULL;
 
-	t_0 = *list;
-	while (t_0)
+	temp = *list;
+	while (temp)
 	{
-		if (sorted_list(t_0) == 1)
+		if (sorted_list(temp) == 1)
 			return;
-
-		while (ctrl == 1)
-		{
-			if (t_0->next == NULL)
-			{
-				ctrl = 0;
-				break;
-			}
-			if (t_0->n <= t_0->next->n)
-				t_0 = t_0->next;
-			else if (t_0->n > t_0->next->n)
-			{
-
-				t_1 = t_0->prev, t_2 = t_0->next, t_3 = t_0->next->next, t_1->next = t_2;
-				t_2->prev = t_1, t_2->next = t_0, t_0->prev = t_2, t_0->next = t_3;
-				t_3 != NULL ? t_3->prev = t_0 : NULL;
-				print_list(*list);
-			}
-		}
-		while (ctrl == 0)
-		{
-			if (t_0->prev == NULL)
-			{
-				ctrl = 1;
-				break;
-			}
-			if (t_0->n >= t_0->prev->n)
-				t_0 = t_0->prev;
-			else if (t_0->n < t_0->prev->n)
-			{
-				t_1 = t_0->next, t_2 = t_0->prev, t_3 = t_0->prev->prev, t_2->next = t_1;
-				t_1->prev = t_2, t_0->next = t_2, t_2->prev = t_0, t_0->prev = t_3;
-				if (t_3 != NULL)
-					t_3->next = t_0;
-				else
-					*list = t_0;
-				print_list(*list);
-			}
-		}
+		forward_sort(list, &temp);
+		backward_sort(list, &temp);
 	}
 }
